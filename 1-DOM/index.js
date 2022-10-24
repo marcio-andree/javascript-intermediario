5e3932b209f61cb3038f80a5f9ac39f4c42c83a8
Marcio André <andremarcio1988@gmail.com>
2022-10-24 10:45:51 -0300

    javascript intermediário  DOM

---
+++ 1-DOM/index.js
@@ -0,0 +1,11 @@
+// pega o documento pelo id
+
+let p1 = document.getElementById("p1");
+
+// pega o documento pela classe
+
+let parafrago = document.getElementsByClassName("parafrago");
+
+// pega o documento pela tag
+
+let tag = document.getElementsByTagName("h1");
