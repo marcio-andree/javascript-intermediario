0cea666c5b6405490a5274aeb710aa05487a3adb
Marcio André <andremarcio1988@gmail.com>
2022-10-25 10:04:14 -0300

    mofificadores de elemento innerHTML, textContent, innerText

---
+++ 2-MOTIFICANDO-ELEMENTOS/index.js
@@ -0,0 +1,7 @@
+// modificadores de elemento
+
+// innerHTML -> recebe elemento html
+
+// innerTEXT retonar um texto visivel
+
+// textContent -> retorna todo texto que está no html
---
+++ 3-EXTRA/index.js
@@ -0,0 +1,9 @@
+function colorir(cor){
+  var caixa = document.getElementById("p1");
+  caixa.style.color = cor;
+}
+
+function cleam(cor){
+  var limpar = document.getElementById("p1");
+  limpar.style.color = cor;
+}
