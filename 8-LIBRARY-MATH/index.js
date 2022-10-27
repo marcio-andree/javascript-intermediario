8a81dbeaaa3443227558515be6d9c22eb0f0bcc6
Marcio André <andremarcio1988@gmail.com>
2022-10-27 09:40:29 -0300

    Library Math

---
+++ 8-LIBRARY-MATH/index.js
@@ -0,0 +1,19 @@
+// PI
+let pi = Math.PI;
+console.log(pi);
+// raiz quadrada
+let raizQuad = Math.sqrt(16);
+console.log(raizQuad);
+
+// potenciação
+
+let pot = Math.pow(2, 2);
+console.log(pot);
+
+// Ramdom gera um número aleatório
+// round arrondonda o número gerado
+// flor arrondada com casas decimais para baixo
+// ceil arronda com casas decimais para cima
+let gerarNum = Math.round(Math.random() * 10);
+
+console.log(gerarNum);
