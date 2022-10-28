bac7f37c99d7230bc97241c445b28c0fcc9c5b7d
Marcio André <andremarcio1988@gmail.com>
2022-10-28 10:35:07 -0300

    sorteador usando library Math

---
+++ 11-PAGE-SORTEIO/index.js
@@ -0,0 +1,21 @@
+let pessoas = ["Pedro", "Arrasca", "Gabi","Everton R.",
+              "joão gomes", "Thiago maia","felipe luiz",
+              "Rodnei", "Leo Pereira", "Davi Luiz", "Santos"
+              ]
+
+
+function sortear() {
+ let numeroPessoas  = pessoas.length;
+ let numSorte = Math.floor(Math.random() * numeroPessoas);
+ document.getElementById("display").innerHTML = pessoas[numSorte];
+}
+
+// Outra forma de fazer
+
+// function sortear() {
+//   let numeroPessoas = 100;
+//   let numSorte = Math.round(Math.random() * numeroPessoas);
+//   document.getElementById("display").innerHTML = numSorte;
+
+// }
+
